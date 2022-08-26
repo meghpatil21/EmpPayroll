07c2d59c5925523d84b9b336664901b46ae0cceb
meghpatil21 <megh.after18@gmail.com>
2022-08-26 22:57:14 +0530

    [megh] UC2-DailyEmpWage

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -12,12 +12,14 @@ namespace EmployeePayroll
             //Console.WriteLine(random.NextDouble()); ==> 0.0 - 1.0
             //Console.WriteLine(random.Next(10,20)); ==> 10 - 19
 
-            Console.WriteLine("Choose option\n 1.Check Attendance");
+            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1: 
                     EmpPresentAbsent.EmployeeAttendanceCheck();
+                    break;
+                case 2: DailyWage.CalculateDailywage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
