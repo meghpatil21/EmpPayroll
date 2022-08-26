c5a0f485182bb12591d79ec9347d4a7a484fc937
meghpatil21 <megh.after18@gmail.com>
2022-08-26 23:20:08 +0530

    [megh] Part time wage

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -12,14 +12,18 @@ namespace EmployeePayroll
             //Console.WriteLine(random.NextDouble()); ==> 0.0 - 1.0
             //Console.WriteLine(random.Next(10,20)); ==> 10 - 19
 
-            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee");
+            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee\n 3.Part time Wage");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1: 
                     EmpPresentAbsent.EmployeeAttendanceCheck();
                     break;
-                case 2: DailyWage.CalculateDailywage();
+                case 2: 
+                    DailyWage.CalculateDailywage();
+                    break;
+                case 3:
+                    ParttimeWage.PartTimeWage();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
