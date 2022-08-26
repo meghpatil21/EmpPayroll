eb9954c56933c6811cd34d26869330201e48c7e5
meghpatil21 <megh.after18@gmail.com>
2022-08-26 22:07:37 +0530

    [megh] welcome msg

---
+++ EmployeePayroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage");
+        }
+    }
+}
