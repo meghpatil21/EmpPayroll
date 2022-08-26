229a81181299e822e26881e10e6c0399ae31c2be
meghpatil21 <megh.after18@gmail.com>
2022-08-26 23:39:03 +0530

    [megh] Switch case

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -12,7 +12,7 @@ namespace EmployeePayroll
             //Console.WriteLine(random.NextDouble()); ==> 0.0 - 1.0
             //Console.WriteLine(random.Next(10,20)); ==> 10 - 19
 
-            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee\n 3.Part time Wage");
+            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee\n 3.Part time Wage\n 4.Wage using Swtich Case");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -24,6 +24,9 @@ namespace EmployeePayroll
                     break;
                 case 3:
                     ParttimeWage.PartTimeWage();
+                    break;
+                case 4:
+                    SwitchCase.SwitchStatement();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
---
+++ EmployeePayroll/SwitchCase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    internal class SwitchCase
+    {
+        public static void SwitchStatement()
+        {
+                   
+            const int EMP_RATE_PER_HOUR = 20;
+
+            int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(1, 3);
+
+            switch(empCheck) 
+            {
+                case 1:
+                empHrs = 8;
+                Console.WriteLine("Emp is Present for full time");
+                    break;
+                case 2:
+                empHrs = 4;
+                Console.WriteLine("Emp is Present for part time");
+                    break ;
+                    default:
+                empHrs = 0;
+                Console.WriteLine("Emp is Absent");
+                    break;
+            }
+            int empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Daily wage is " + empWage);
+        }
+    }
+}
