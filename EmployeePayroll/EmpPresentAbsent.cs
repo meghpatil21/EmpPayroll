75e08d3764d2e66c9b0193e9445785e336553a68
meghpatil21 <megh.after18@gmail.com>
2022-08-26 22:35:31 +0530

    [megh] UC1-Employee present or absent

---
+++ EmployeePayroll/EmpPresentAbsent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll
+{
+    internal class EmpPresentAbsent
+    {
+        public static void EmployeeAttendanceCheck()
+        {
+            Random random = new Random();
+            int empCheck=random.Next(0, 2);
+            if(empCheck == 0)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -7,6 +7,24 @@ namespace EmployeePayroll
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage");
+            //Random random = new Random();
+            //Console.WriteLine(random.Next(2)); //0-1
+            //Console.WriteLine(random.NextDouble()); ==> 0.0 - 1.0
+            //Console.WriteLine(random.Next(10,20)); ==> 10 - 19
+
+            Console.WriteLine("Choose option\n 1.Check Attendance");
+            int option=Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1: 
+                    EmpPresentAbsent.EmployeeAttendanceCheck();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong choice");
+                    break;
+            }
+
+
         }
     }
 }
