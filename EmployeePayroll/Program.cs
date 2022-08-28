f594776f1dc64c4c918cdf3526bd6632b5017cf5
meghpatil21 <megh.after18@gmail.com>
2022-08-28 18:14:30 +0530

    [megh] total working hrs

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -12,7 +12,7 @@ namespace EmployeePayroll
             //Console.WriteLine(random.NextDouble()); ==> 0.0 - 1.0
             //Console.WriteLine(random.Next(10,20)); ==> 10 - 19
 
-            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee\n 3.Part time Wage\n 4.Wage for month");
+            Console.WriteLine("Choose option\n 1.Check Attendance\n 2.Daily Wage of Employee\n 3.Part time Wage\n 4.Wage for month\n 5.20 Days or 100 Hrs");
             int option=Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -27,6 +27,9 @@ namespace EmployeePayroll
                     break;
                 case 4:
                     WageForMonth.CalculateWageForMonth();
+                    break;
+                case 5:
+                    WorkHrsDayLimit.WrkHrsDay();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong choice");
